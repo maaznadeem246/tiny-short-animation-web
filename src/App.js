70333a4f59669db135db4455a9494a896eb0aedc
Maaz <maaznadeem246@gmail.com>
2020-07-26 21:45:18 +0500

    created header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header";
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "'Libre Baskerville', serif",
+    color:'#18242A',
+  },
+  
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-        Tiny Short Animation Website.
+        <Header />
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { display } from "@material-ui/system";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+
+    },
+    appBar:{
+        backgroundColor: 'white',
+        color: theme.typography.color,
+        display:'flex',
+     
+
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        padding:10,
+        fontSize:25,
+        flexGrow:1,
+    },
+    links:{
+        fontSize: 20,
+        padding:'10px 50px 10px 50px',
+        flexGrow: 3,
+        '& > *': {
+            textDecoration:'none',
+            marginLeft: theme.spacing(2),
+            color: theme.typography.color,
+            paddingRight:30,
+            paddingLet:30,
+            '&:hover':{
+                textDecoration: 'none',
+            },
+            '&:active':{
+                color:'inherit'
+            }
+        },
+    }
+}));
+
+
+function Header(){
+    const classes = useStyles();
+    const preventDefault = (event) => event.preventDefault();
+
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" >
+                <Toolbar className={classes.appBar}>
+                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" className={classes.title}>
+                       Future Technologies
+                    </Typography>
+                    <Typography className={classes.links}>
+                        <Link href="#" onClick={preventDefault}>
+                            Home
+                        </Link>
+                        <Link href="#" onClick={preventDefault} >
+                            About
+                        </Link>
+                        <Link href="#" onClick={preventDefault}>
+                            Blog
+                        </Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Header;
