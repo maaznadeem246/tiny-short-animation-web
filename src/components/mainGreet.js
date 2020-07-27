c39d15853a34ee09405558fd3a8162223b858a18
Maaz <maaznadeem246@gmail.com>
2020-07-27 22:51:34 +0500

    some animation changes

--- src/components/mainGreet.js
+++ src/components/mainGreet.js
@@ -7,75 +7,137 @@ const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
+
+    animatedText:{
+        textAlign:'left',
+        paddingTop:60,
+        paddingBottom:30,
+        paddingRight:30,
+        paddingLeft: 30,
+        fontSize:30,
+        fontWeight:700,
+        color:theme.typography.color,
+    },
+    animatedLine:{
+        overflow:'hidden',
+        paddingTop:4,
+        paddingBottom:4,
+    },
+    animatedBottomDiv:{
+        display:'flex',
+        fontSize: 17,
+        paddingTop:60,
+        justifyContent:'space-between',
+    },
+    animatedBottomLine:{
+        overflow:'hidden'
+    }
+
   
 }));
 
 
 function AnimatedText(){
-    
-    const textArray = [
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    const classes = useStyles();
+    const firstTextArray = [
+        'Lorem Ipsum is simply dummy text ',
+        'Lorem Ipsum is simply ',
+        'Lorem Ipsum is simply dummy '
+    ]
+    const secondTextArray = [
         'Lorem Ipsum is simply dummy text of the.',
-        'Lorem Ipsum is simply dummy text of the second.'
+        'Lorem Ipsum is simply dummy text',
+        'Lorem Ipsum is simply ',
+        
+    ]
+    const thirdTextArray = [
+        'Lorem Ipsum is simply',
+        'Lorem Ipsum is simply dummy',
+        'Lorem Ipsum is simply dummy text of the.',
     ]
     const [count, updateCount] = useState(0)    
-    const [text, updateText] = useState('')
+    const [text, updateText] = useState({
+        firstLine:'',
+        secondLine:'',
+        thirdLine:'',
+    })
    
-    const { ref, playState, getAnimation, animate } = useWebAnimations({
-        keyframes: [
-            {
-                offset: 0,
-                transform: "translate3d(0, 100%, 0)",
-                visibility: "visible"
-            },
-            {
-                offset: 0.1,
-                transform: "translate3d(0, 0, 0)",
-                visibility: "visible"
-            },
-            {
-                offset: 0.9,
-                transform: "translate3d(0, 0, 0)",
-                visibility: "visible"
-            },
-            {
-                offset: 1,
-                transform: "translate3d(0, 100%, 0)",
-                visibility: "hidden"
-            }
 
-        ]
-        ,
-        timing: {
-
-            duration: 4000,
-            fill: "both",
-
-        },
-        autoPlay: false,
-        onUpdate: ({ playState, animate, animation }) => {
-            // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-            if (playState === "finished") {
-                // console.log(count)
-                if(count < 2){
-                // console.log('d')
-                updateCount((count) => count + 1)
-                }else{
-                    // console.log('dd')
-                    updateCount(0)
+    const animationObject = (delayTime, animateOffset) => {
+        return {
+            keyframes: [
+                {
+                    offset: 0,
+                    transform: "translate3d(0, 100%, 0)",
+                    visibility: "visible"
+                },
+                {
+                    offset: animateOffset,
+                    transform: "translate3d(0, 0, 0)",
+                    visibility: "visible"
+                },
+                {
+                    offset: 0.9,
+                    transform: "translate3d(0, 0, 0)",
+                    visibility: "visible",
+                    opacity:1,
+                },
+                {
+                    opacity:0.7
+                },
+                {
+                    opacity: 0.3
+                },
+                {
+                    offset: 1,
+                    transform: "translate3d(0, 100%, 0)",
+                    visibility: "hidden"
                 }
 
-                // console.log('finished')
-            }
-            
-        },
-    });
+            ]
+            ,
+            timing: {
+                delay:delayTime,
+                duration: 4000 - delayTime,
+                fill: "both",
+
+            },
+            autoPlay: false,
+            onUpdate: ({ playState, animate, animation }) => {
+                // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+                if (playState === "finished") {
+
+                    if (count < 2) {
+                        updateCount((count) => count + 1)
+                    } else {
+                        updateCount(0)
+                    }
+                }
+
+            },
+        }
+    }
+
+    const firstLine = useWebAnimations(animationObject(50,0.1));
+
+    const secondLine = useWebAnimations(animationObject(200,0.1));
+
+    const thirdLine = useWebAnimations(animationObject(300,0.1));
+
+    const bottomLine = useWebAnimations(animationObject(300, 0.2));
 
 
     useEffect(()=>{
         // animate()
-        updateText(textArray[count])
-        getAnimation().play();
+        updateText({
+            firstLine:firstTextArray[count],
+            secondLine:secondTextArray[count],
+            thirdLine: thirdTextArray[count],
+        })
+        firstLine.getAnimation().play();
+        secondLine.getAnimation().play();
+        thirdLine.getAnimation().play();
+        bottomLine.getAnimation().play();
         console.log('effect')
     },[count])
 
@@ -83,8 +145,24 @@ function AnimatedText(){
 
 //    console.log(slideInUp)
     return(
-        <div style={{overflow:'hidden'}}>
-            <div ref={ref}>{text}</div>
+        <div className={classes.animatedText}>
+            <div className={classes.animatedLine}>
+                <div ref={firstLine.ref}>{text.firstLine}</div>
+            </div>
+            <div className={classes.animatedLine}>
+                <div ref={secondLine.ref}>{text.secondLine}</div>
+            </div>
+            <div className={classes.animatedLine}>
+                <div ref={thirdLine.ref}>{text.thirdLine}</div>
+            </div>
+            <div className={classes.animatedLine}>
+                <div className={classes.animatedBottomDiv}>
+                    <div className={classes.animatedBottomLine} >
+                        <div ref={bottomLine.ref}>Bottom Text</div>
+                    </div>
+                    <div>buttons</div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -96,8 +174,8 @@ function MainGreet(){
     return (
         <div className={classes.root}>
             <Grid container >
-                <Grid item xs={12} sm={12} md={5} lg={5}>1</Grid>
-                <Grid item xs={12} sm={12} md={7} lg={7}>
+                <Grid item xs={12} sm={12} md={6} lg={6}>1</Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
                     <AnimatedText />
                 </Grid>
             </Grid>
