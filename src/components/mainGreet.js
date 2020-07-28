5fc6b962ab01db7400dcbef9cfc2f706782c5366
Maaz <maaznadeem246@gmail.com>
2020-07-28 19:31:11 +0500

    some changes

--- src/components/mainGreet.js
+++ src/components/mainGreet.js
@@ -2,6 +2,9 @@ import React,{useEffect,useState} from "react"
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import useWebAnimations, {slideInUp,slideOutDown} from "@wellyshen/use-web-animations";
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -31,6 +34,41 @@ const useStyles = makeStyles((theme) => ({
     },
     animatedBottomLine:{
         overflow:'hidden'
+    },
+    bottomAandB:{
+        display:'flex',
+        alignItems:'center',
+    },
+    arrowButton:{
+        fontSize:'2rem',
+    },
+    arrowButtonBack:{
+        transform:'rotate(180deg)'
+    },
+
+    smallButtons:{
+        width:9,
+        height:9,
+        margin:6,
+        border: '1px solid' + theme.typography.color,
+        borderRadius:1.5,
+        opacity:0.4,
+        transform: 'rotate(139deg)',
+        backgroundColor:theme.typography.color,
+        transition:'opacity 0.4s linear',
+        "&:hover":{
+            opacity: 1,
+        }
+    },
+    smallButtonsGroup:{
+        display:'flex',
+        paddingLeft:20,
+        paddingRight:20,
+    },
+    backgroundAnimated:{
+        height:'100%',
+        width:'100%',
+
     }
 
   
@@ -40,9 +78,9 @@ const useStyles = makeStyles((theme) => ({
 function AnimatedText(){
     const classes = useStyles();
     const firstTextArray = [
-        'Lorem Ipsum is simply dummy text ',
-        'Lorem Ipsum is simply ',
-        'Lorem Ipsum is simply dummy '
+        '0Lorem Ipsum is simply dummy text ',
+        '1Lorem Ipsum is simply ',
+        '2Lorem Ipsum is simply dummy '
     ]
     const secondTextArray = [
         'Lorem Ipsum is simply dummy text of the.',
@@ -72,19 +110,12 @@ function AnimatedText(){
                     visibility: "visible"
                 },
                 {
-                    transform: "translate3d(0, 20%, 0)",
-                },
-            
-                {
-                    transform: "translate3d(0, 10%, 0)",
-                },
-                {
                     offset: animateOffset,
                     transform: "translate3d(0, 0, 0)",
                     visibility: "visible"
                 },
                 {
-                    offset: 0.9,
+                    offset: 0.92,
                     transform: "translate3d(0, 0, 0)",
                     visibility: "visible",
                     opacity:1,
@@ -105,16 +136,21 @@ function AnimatedText(){
             ,
             timing: {
                 delay:delayTime,
-                duration: 4000 - delayTime,
+                duration: 4500 - delayTime,
                 fill: "both",
-
+                ease:'ease'
+//cubic-bezier(0.0, 0.0, 1.0, 0.95)
             },
             autoPlay: false,
-            onUpdate: ({ playState, animate, animation }) => {
+            onUpdate: ({ playState, animate, animation: { pending }}) => {
                 // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-                if (playState === "finished") {
+               
+                if (playState === "finished" && !pending) {
+                    // console.log(playState)
+                    // console.log(pending)
 
                     if (count < 2) {
+                        // console.log(count)
                         updateCount((count) => count + 1)
                     } else {
                         updateCount(0)
@@ -122,37 +158,71 @@ function AnimatedText(){
                 }
 
             },
+
         }
     }
 
-    const firstLine = useWebAnimations(animationObject(50,0.1));
+    const firstLine = useWebAnimations(animationObject(100,0.1));
 
-    const secondLine = useWebAnimations(animationObject(200,0.2));
+    const secondLine = useWebAnimations(animationObject(150,0.2));
 
-    const thirdLine = useWebAnimations(animationObject(300,0.3));
+    const thirdLine = useWebAnimations(animationObject(200,0.3));
 
-    const bottomLine = useWebAnimations(animationObject(400, 0.4));
+    const bottomLine = useWebAnimations(animationObject(250, 0.35));
 
 
     useEffect(()=>{
         // animate()
+       // console.log(firstTextArray[count])
         updateText({
             firstLine:firstTextArray[count],
             secondLine:secondTextArray[count],
             thirdLine: thirdTextArray[count],
         })
+        //console.log('effect')
         firstLine.getAnimation().play();
         secondLine.getAnimation().play();
         thirdLine.getAnimation().play();
         bottomLine.getAnimation().play();
-        console.log('effect')
+  
     },[count])
 
+    const changeItButton = (num) => {
+        firstLine.getAnimation().finish();
+        secondLine.getAnimation().finish();
+        thirdLine.getAnimation().finish();
+        bottomLine.getAnimation().finish();
 
+        updateCount(num)
+    }
+    const changeIt = (v) => {
+      
+        firstLine.getAnimation().finish();
+        secondLine.getAnimation().finish();
+        thirdLine.getAnimation().finish();
+        bottomLine.getAnimation().finish();
+        if(v=='next'){
+
+            if (count < 2) {
+                updateCount((count) => count + 1)
+            } else {
+                updateCount(0)
+            }
+        }else if(v=='back'){
+            if (count <= 2 && count > 0) {
+                updateCount((count) => count - 1)
+            } else {
+                updateCount(2)
+            }
+        }
+    }
 
 //    console.log(slideInUp)
     return(
+        <div>
+        <div className={classes.backgroundAnimated} />
         <div className={classes.animatedText}>
+ 
             <div className={classes.animatedLine}>
                 <div ref={firstLine.ref}>{text.firstLine}</div>
             </div>
@@ -164,12 +234,22 @@ function AnimatedText(){
             </div>
             <div className={classes.animatedLine}>
                 <div className={classes.animatedBottomDiv}>
-                    <div className={classes.animatedBottomLine} >
-                        <div ref={bottomLine.ref}>Bottom Text</div>
+                    <div  >
+                        <div className={classes.animatedBottomLine} ><div ref={bottomLine.ref}>Bottom Text</div></div>
                     </div>
-                    <div>buttons</div>
+                    <div className={classes.bottomAandB}>
+                            <div className={classes.smallButtonsGroup}>
+                                <div onClick={() => { changeItButton(0)}} className={classes.smallButtons}  style={count == 0 ? {opacity:1} : {}} ></div>
+                                <div onClick={() => { changeItButton(1) }} className={classes.smallButtons} style={count == 1 ? { opacity: 1 } : {}} ></div>
+                                <div onClick={() => { changeItButton(2) }} className={classes.smallButtons} style={count == 2 ? { opacity: 1 } : {}} ></div>
+                            </div>
+                            <ArrowBackIosIcon className={classes.arrowButtonNext} onClick={()=>{changeIt('back')}} />                   
+                            <ArrowBackIosIcon className={classes.arrowButtonBack} onClick={() => { changeIt('next')}}/>
+                       
+                    </div>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
@@ -185,8 +265,7 @@ function MainGreet(){
                 <Grid item xs={12} sm={12} md={6} lg={6}>
                     <AnimatedText />
                 </Grid>
-            </Grid>
-            
+            </Grid>            
         </div>
     )
 }
